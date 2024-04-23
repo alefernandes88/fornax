54037446741618fbc9b20744d48e6ee7eb333213
sandroex <sandroex_compras@hotmail.com>
2024-04-23 05:47:26 -0300

    Initial Test

--- Fornax.Test/Controllers/UseControllerTest.cs
+++ Fornax.Test/Controllers/UseControllerTest.cs
@@ -1,12 +1,68 @@
-﻿using System;
+﻿using FluentAssertions;
+using fornax.Controllers;
+using fornax.Core.Entities;
+using fornax.Usecase.UserCase.Query;
+using Fornax.Test.MockData;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace Fornax.Test.Controllers
 {
-    internal class UseControllerTest
+    public class UseControllerTest
     {
+        [Fact]
+        public void getUserReturn200Status() 
+        {
+            var userGetQuery = new Mock<UserGetQuery>();
+            userGetQuery.Setup(_ => _.getUser(It.IsAny<int>())).Returns(UserMockData.getUser()[0]);
+            var controller = new UserController();
+
+            var result = (OkObjectResult)controller.GetUser(1);
+
+            result.StatusCode.Should().Be(200);
+
+        }
+
+        [Fact]
+        public void getUserReturn400Status()
+        {
+            var userGetQuery = new Mock<UserGetQuery>();
+            userGetQuery.Setup(_ => _.getUser(It.IsAny<int>())).Returns(new User());
+            var controller = new UserController();
+
+            var result = (NotFoundResult)controller.GetUser(1);
+
+            result.StatusCode.Should().Be(400);
+        }
+
+        [Fact]
+        public void getAllReturn200Status()
+        {
+            var userGetAllQuery = new Mock<UserGetAllQuery>();
+            userGetAllQuery.Setup(x => x.getAll()).Returns(UserMockData.getUser());
+            var controller = new UserController();
+
+            var result = (OkObjectResult)controller.GetAll();
+
+            result.StatusCode.Should().Be(200);
+        }
+
+        [Fact]
+        public void getAllReturn404Status()
+        {
+            var userGetAllQuery = new Mock<UserGetAllQuery>();
+            userGetAllQuery.Setup(x => x.getAll()).Returns([]);
+            var controller = new UserController();
+
+            var result = (NotFoundResult)controller.GetAll();
+
+            result.StatusCode.Should().Be(404);
+        }
     }
 }
---
+++ Fornax.Test/MockData/UserMockData.cs
@@ -0,0 +1,35 @@
+﻿using fornax.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fornax.Test.MockData
+{
+    internal class UserMockData
+    {
+        public static List<User> getUser()
+        {
+            return new List<User>()
+            {
+                new User
+                {
+                    Name = "Test",
+                    Email = "teste@teste.com",
+                    Id = 1,
+                    Pass = "123456",
+                    InitialDate = DateTime.Now,
+                },
+                new User
+                {
+                    Name = "Test2",
+                    Email = "teste2@teste.com",
+                    Id = 2,
+                    Pass = "123456",
+                    InitialDate = DateTime.Now,
+                }
+            };
+        }
+    }
+}
